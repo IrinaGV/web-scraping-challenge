8c865ffab8b001a19780e364d119125146f73196
IrinaGV <79545724+IrinaGV@users.noreply.github.com>
2021-09-29 20:49:23 -0500

    final

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,42 +1,63 @@
-from splinter import Browser
-from bs4 import BeautifulSoup
-import pandas as pd
+from bs4 import BeautifulSoup as bs
 import requests
-import pymongo
+from splinter import Browser
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
 
-def init_browser():
-    executable_path = {"executable_path": "chromedriver.exe"}
-    return Browser("chrome", **executable_path, headless=False)
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
 
 def scrape():
-    browser = init_browser()
-    mars_dict ={}
+    scraped_result = {}
+    news= mars_news()
+    scraped_result["mars_news"] = news[0]
+    scraped_result["mars_paragraph"] = news[1]
+    scraped_result["mars_image"] = mars_image()
+    scraped_result["mars_facts"] = mars_facts()
+    scraped_result["mars_hemisphere"] = mars_h_data()
+    browser.quit()
+    return scraped_result
 
-    mars_site = 'https://redplanetscience.com/'
-    browser.visit(mars_site)
-    mars_html = browser.html
-    mars_soup = bs(mars_html, 'html.parser')
+
+def mars_news():
     
-    news_title = mars_soup.find('div', class_='content_title').get_text()
+    mars_site= "https://redplanetscience.com/"
+    browser.visit(mars_site)
+    
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    news_title = mars_soup.find_all('div', class_='content_title')[0].text
     news_p = mars_soup.find_all('div', class_='article_teaser_body')[0].text
+    news = [news_title, news_p]
+    return news
 
 
-    mars_image = 'https://spaceimages-mars.com/'
+def  mars_image():
+    mars_image = "https://spaceimages-mars.com/"
     browser.visit(mars_image)
     html = browser.html
     images_soup = bs(html, 'html.parser')
     
     image_path= images_soup.find_all('img', class_='headerimage fade-in')[0]['src']
     featured_image_url = mars_image + image_path 
+    print(featured_image_url)
+    return featured_image_url
 
 
-    mars_facts = 'https://galaxyfacts-mars.com/'
-    fact_tables = pd.read_html(mars_facts)
+def  mars_facts():
+    mars_facts_url = "https://galaxyfacts-mars.com/"
+    fact_tables = pd.read_html(mars_fact_url)
     mars_factsDF = fact_tables[1]
-    mars_factsDF.columns = ["Characteristic", "Fact"]
+    fact_tables.columns = ['Characteristics', 'Fact']
     mars_html_table_string = mars_factsDF.to_html()
-    
-    
+    # print(mars_facts)
+    return mars_html_table_string
+
+
+def mars_h_data():
     mars_hemisperes = 'https://marshemispheres.com/'
     browser.visit(mars_hemisperes)
     hemispheres_html = browser.html
@@ -44,26 +65,25 @@ def scrape():
     
     hemisphere_image_urls = []
     mars_hemispheres_descr = hemispheres_soup.find_all('div', class_='description')
-    
+
     for i in mars_hemispheres_descr:
         title = i.find("h3").text
         hemispheres_image = i.find("a", class_="itemLink product-item")["href"]
+        
         browser.visit(mars_hemisperes + hemispheres_image)
-    
+           
         image_html = browser.html
         info = bs(image_html, "html.parser")
-    
+        
         image_url = mars_hemisperes + info.find("img", class_="wide-image")["src"]
         hemisphere_image_urls.append({"title" : title, "image_url" : image_url})
 
+        print("")
+        print(title)
+        print(image_url)
+        print("--------------------")
+    return hemisphere_image_urls 
 
-     
-    mars_dict = {
-        "news_title": news_title,
-        "news_p": news_p,
-        "featured_image_url": featured_image_url,
-        "mars_fact": str(mars_html_table_string),
-        "hemisphere_images":  hemisphere_image_urls
-    }
-
-    return mars_dict
+    
+if __name__ == "__main__":
+    print(scrape())
